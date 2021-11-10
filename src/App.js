1239d7b396c0cb7db7d30718049a3d6a87d95eaf
Babken2005 <babkengevorgyan2005@gmail.com>
2021-11-10 19:28:31 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="content">
+      <div className="home">
+      <h1 className="wave">👋</h1>
+      <header className="a-c">
+        <div className="headerLeft">
+        <p>B<span>.</span></p>
+        </div>
+        <div className="headerRight">
+          <p>say hi.</p>
+        </div>
       </header>
+      <div className="homeContent a-c j-c">
+        <div className="homeContentParent">
+        <div className="block-1 contentTextBlock a-c"><h1>Unique<span>.</span></h1></div>
+        <div className="block-2 contentTextBlock a-c"><h1>Gifted<span>.</span></h1></div>
+        <div className="block-3 contentTextBlock a-c"><h1>Creative<span>.</span></h1></div>
+        <div className="block-4 contentTextBlock a-c"><h1>Individual<span>.</span></h1></div>
+        </div>
+      </div>
+      <footer className="a-c">
+        <div className="footerLeft">
+          <img src="./images/github.svg" alt="github" />
+          <img src="./images/instagram.svg" alt="instagram" />          
+          <img src="./images/twitter.svg" alt="twitter" />
+          <img src="./images/facebook.svg" alt="facebook" />
+        </div>
+        <div className="footerRight">
+          <p>works.</p>
+        </div>
+      </footer>
+      </div>
     </div>
   );
 }
