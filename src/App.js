3ff6fd96a1459473f5aa362d3b96c84f15dd439d
Babken <76743515+Babken2005@users.noreply.github.com>
2021-11-10 21:48:40 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="content">
       <div className="home">
-      <h1 className="wave">👋</h1>
+      <h1 data-aos="fade-up" data-aos-duration="2400" className="wave">👋</h1>
       <header className="a-c">
         <div className="headerLeft">
         <p>B<span>.</span></p>
@@ -15,10 +15,10 @@ function App() {
       </header>
       <div className="homeContent a-c j-c">
         <div className="homeContentParent">
-        <div className="block-1 contentTextBlock a-c"><h1>Unique<span>.</span></h1></div>
-        <div className="block-2 contentTextBlock a-c"><h1>Gifted<span>.</span></h1></div>
-        <div className="block-3 contentTextBlock a-c"><h1>Creative<span>.</span></h1></div>
-        <div className="block-4 contentTextBlock a-c"><h1>Individual<span>.</span></h1></div>
+        <div className="block-1 contentTextBlock a-c"><div className="block-1-parent textParent a-c"><div data-aos="fade-up" data-aos-duration="1500">U</div><div data-aos="fade-up" data-aos-duration="1600">n</div><div data-aos="fade-up" data-aos-duration="1700">i</div><div data-aos="fade-up" data-aos-duration="1800">q</div><div data-aos="fade-up" data-aos-duration="1900">u</div><div data-aos="fade-up" data-aos-duration="2000">e<span>.</span></div></div></div>
+        <div className="block-2 contentTextBlock a-c"><div className="block-2-parent textParent a-c"><div data-aos="fade-up" data-aos-duration="1500">G</div><div data-aos="fade-up" data-aos-duration="1600">i</div><div data-aos="fade-up" data-aos-duration="1700">f</div><div data-aos="fade-up" data-aos-duration="1800">t</div><div data-aos="fade-up" data-aos-duration="1900">e</div><div data-aos="fade-up" data-aos-duration="2000">d<span>.</span></div></div></div>
+        <div className="block-3 contentTextBlock a-c"><div className="block-3-parent textParent a-c"><div data-aos="fade-up" data-aos-duration="1500">C</div><div data-aos="fade-up" data-aos-duration="1600">r</div><div data-aos="fade-up" data-aos-duration="1700">e</div><div data-aos="fade-up" data-aos-duration="1800">a</div><div data-aos="fade-up" data-aos-duration="1900">t</div><div data-aos="fade-up" data-aos-duration="2000">i</div><div data-aos="fade-up" data-aos-duration="2100">v</div><div data-aos="fade-up" data-aos-duration="2200">e<span>.</span></div></div></div>
+        <div className="block-4 contentTextBlock a-c"><div className="block-4-parent textParent a-c"><div data-aos="fade-up" data-aos-duration="1500">I</div><div data-aos="fade-up" data-aos-duration="1600">n</div><div data-aos="fade-up" data-aos-duration="1700">d</div><div data-aos="fade-up" data-aos-duration="1800">i</div><div data-aos="fade-up" data-aos-duration="1900">v</div><div data-aos="fade-up" data-aos-duration="2000">i</div><div data-aos="fade-up" data-aos-duration="2100">d</div><div data-aos="fade-up" data-aos-duration="2200">u</div><div data-aos="fade-up" data-aos-duration="2300">a</div><div data-aos="fade-up" data-aos-duration="2400">l<span>.</span></div></div></div>
         </div>
       </div>
       <footer className="a-c">
